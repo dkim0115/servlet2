0d5fa7b3dd007333492a34cb66dff8576182e2c2
bookck13 <bookck13@gmail.com>
2023-06-02 18:23:45 -0400

    Initial commit

---
+++ src/test/java/com/example/servlet2/Servlet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servlet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servlet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
