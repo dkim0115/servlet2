00add76f6f90ff83ea4726855a16a92b02cf1d9d
bookck13 <bookck13@gmail.com>
2023-06-07 04:59:50 -0400

    commit

---
+++ src/main/java/com/example/servlet2/basic/request/RequestParamServlet.java
@@ -0,0 +1,36 @@
+package com.example.servlet2.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("start");
+        request.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+        System.out.println("end");
+        System.out.println();
+
+        System.out.println("single param search");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+
+        System.out.println("mutiple param search");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username = " + name);
+        }
+
+        response.getWriter().write("ok");
+
+    }
+}
