268086c58cdea6216eae58fe64cd37cb3d0ba384
bookck13 <bookck13@gmail.com>
2023-06-09 12:08:19 -0400

    commit

---
+++ src/main/java/com/example/servlet2/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,8 @@
+package com.example.servlet2.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    String process(Map<String, String> paramMpa, Map<String, Object> model);
+}
